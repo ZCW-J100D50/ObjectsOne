eb9b4b6a009d5bd5838eee7c472f3c84bb4e1d91
ShailyGH <159935606+ShailyGH@users.noreply.github.com>
2024-02-27 21:40:42 -0500

    Update Main.java

--- object1/src/main/java/object1/Main.java
+++ object1/src/main/java/object1/Main.java
@@ -7,5 +7,7 @@ public class Main {
     public static void main(String... args) {
         Person newPerson1 = new Person("Doe John",30, 150);
         Logger logger=Logger.getLogger(Main.class.getName());
-        logger.info("This is a module-using Hello World!");    }
+        logger.info("This is a module-using Hello World!");
+        System.out.prntln (newPerson1.height);
+    }
 }
