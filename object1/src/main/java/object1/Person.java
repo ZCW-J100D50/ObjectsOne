05814a7fd4160d1bc6f6ca69e64e9b8278b19c46
ShailyGH <159935606+ShailyGH@users.noreply.github.com>
2024-02-28 14:31:34 -0500

    Update Person.java

--- object1/src/main/java/object1/Person.java
+++ object1/src/main/java/object1/Person.java
@@ -22,39 +22,3 @@ class Person {
         
     }
 }
-
-public class Chair {
-    private String material;
-    private String color;
-    private int legs;
-
-    public Chair(String material, String color, int legs) {
-        this.material = material;
-        this.color = color;
-        this.legs = legs;
-    }
-
-    public String getMaterial() {
-        return material;
-    }
-
-    public void setMaterial(String material) {
-        this.material = material;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public int getLegs() {
-        return legs;
-    }
-
-    public void setLegs(int legs) {
-        this.legs = legs;
-    }
-}
